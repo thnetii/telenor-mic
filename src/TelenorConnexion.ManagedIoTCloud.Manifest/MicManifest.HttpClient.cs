3f80c0f2d545dec17a7d13bc32233e279f3d109e
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-09-18 18:19:48 +0200

    MIC Manifest definition

---
+++ src/TelenorConnexion.ManagedIoTCloud.Manifest/MicManifest.HttpClient.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TelenorConnexion.ManagedIoTCloud
+{
+    public partial class MicManifest
+    {
+        private static readonly JsonSerializer serializer = JsonSerializer.CreateDefault();
+
+        public const string ManifestServiceUrl = "https://1u31fuekv5.execute-api.eu-west-1.amazonaws.com/prod/manifest/";
+
+        public static Uri ManifestServiceUri { get; } = new Uri(ManifestServiceUrl);
+
+        public static Task<MicManifest> GetMicManifest(string hostname, CancellationToken cancellationToken = default) =>
+            GetMicManifest(hostname, httpClient: null, cancellationToken);
+
+        public static Task<MicManifest> GetMicManifest(string hostname, HttpClient httpClient, CancellationToken cancellationToken = default) =>
+            GetMicManifest(ManifestServiceUri, hostname, httpClient, cancellationToken);
+
+        [SuppressMessage("Usage", "CA2234:Pass system uri objects instead of strings")]
+        public static Task<MicManifest> GetMicManifest(string manifestServiceUrl, string hostname, CancellationToken cancellationToken = default) =>
+            GetMicManifest(manifestServiceUrl, hostname, httpClient: null, cancellationToken);
+
+        public static Task<MicManifest> GetMicManifest(Uri manifestServiceUri, string hostname, CancellationToken cancellationToken = default) =>
+            GetMicManifest(manifestServiceUri, hostname, httpClient: null, cancellationToken);
+
+        public static Task<MicManifest> GetMicManifest(string manifestServiceUrl, string hostname, HttpClient httpClient, CancellationToken cancellationToken = default)
+            => GetMicManifest(new Uri(manifestServiceUrl), hostname, httpClient, cancellationToken);
+
+        public static Task<MicManifest> GetMicManifest(Uri manifestServiceUri, string hostname, HttpClient httpClient, CancellationToken cancellationToken = default)
+        {
+            if (manifestServiceUri == null)
+                throw new ArgumentNullException(nameof(manifestServiceUri));
+            if (httpClient == null)
+            {
+                using (httpClient = new HttpClient())
+                {
+                    return GetMicManifestInternal(manifestServiceUri, hostname, httpClient, cancellationToken);
+                }
+            }
+
+            return GetMicManifestInternal(manifestServiceUri, hostname, httpClient, cancellationToken);
+        }
+
+        private static async Task<MicManifest> GetMicManifestInternal(Uri manifestServiceUri, string hostname, HttpClient httpClient, CancellationToken cancellationToken = default)
+        {
+            var requestUri = new Uri(manifestServiceUri, "?hostname=" + Uri.EscapeDataString(hostname));
+            using (var response = await httpClient.GetAsync(requestUri, cancellationToken).ConfigureAwait(continueOnCapturedContext: false))
+            using (var contentStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(continueOnCapturedContext: false))
+            using (var contentTextReader = new StreamReader(contentStream, Encoding.UTF8))
+            using (var jsonReader = new JsonTextReader(contentTextReader))
+            {
+                return serializer.Deserialize<MicManifest>(jsonReader);
+            }
+        }
+    }
+}
---
+++ test/TelenorConnexion.ManagedIoTCloud.Test/Manifest/Test/MicManifestTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using TelenorConnexion.ManagedIoTCloud.Manifest.Data;
+using Xunit;
+
+namespace TelenorConnexion.ManagedIoTCloud.Manifest.Test
+{
+    public class MicManifestTest
+    {
+        public static IEnumerable<object[]> GetSampleFiles() =>
+            EmbeddedData.GetFiles().Select(fi => new[] { fi });
+
+        private static void AssertMicManifest(MicManifest manifest)
+        {
+            Assert.NotNull(manifest);
+            foreach (var pi in manifest.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                object pv = pi.GetValue(manifest);
+                Assert.NotNull(pv);
+            }
+        }
+
+        [Theory, MemberData(nameof(GetSampleFiles))]
+        public static void CanDeserializeSampleData(IFileInfo sampleFile)
+        {
+            var serializer = JsonSerializer.Create();
+
+            using (var stream = sampleFile.CreateReadStream())
+            using (var textReader = new StreamReader(stream, Encoding.UTF8))
+            using (var jsonReader = new JsonTextReader(textReader))
+                AssertMicManifest(serializer.Deserialize<MicManifest>(jsonReader));
+        }
+
+        [SkippableTheory]
+        [InlineData("startiot.mic.telenorconnexion.com")]
+        public static void FetchMicManifestForHostname(string hostname)
+        {
+            var manifest = MicManifest.GetMicManifest(hostname)
+                .GetAwaiter().GetResult();
+            AssertMicManifest(manifest);
+        }
+    }
+}
