4c99e3a83b19b2414afcbd940b7c23d0c520157d
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-09-19 00:24:34 +0200

    Fix premature dispose of MIC manifest HTTP Client

--- src/TelenorConnexion.ManagedIoTCloud.Manifest/MicManifest.HttpClient.cs
+++ src/TelenorConnexion.ManagedIoTCloud.Manifest/MicManifest.HttpClient.cs
@@ -27,28 +27,55 @@ namespace TelenorConnexion.ManagedIoTCloud
         public static Task<MicManifest> GetMicManifest(string manifestServiceUrl, string hostname, CancellationToken cancellationToken = default) =>
             GetMicManifest(manifestServiceUrl, hostname, httpClient: null, cancellationToken);
 
-        public static Task<MicManifest> GetMicManifest(Uri manifestServiceUri, string hostname, CancellationToken cancellationToken = default) =>
-            GetMicManifest(manifestServiceUri, hostname, httpClient: null, cancellationToken);
+        public static Task<MicManifest> GetMicManifest(
+            Uri manifestServiceUri, string hostname,
+            CancellationToken cancellationToken = default)
+        {
+            return GetMicManifest(
+                manifestServiceUri, hostname,
+                httpClient: null, cancellationToken
+                );
+        }
 
-        public static Task<MicManifest> GetMicManifest(string manifestServiceUrl, string hostname, HttpClient httpClient, CancellationToken cancellationToken = default)
-            => GetMicManifest(new Uri(manifestServiceUrl), hostname, httpClient, cancellationToken);
+        public static Task<MicManifest> GetMicManifest(
+            string manifestServiceUrl, string hostname, HttpClient httpClient,
+            CancellationToken cancellationToken = default)
+        {
+            return GetMicManifest(
+                new Uri(manifestServiceUrl), hostname,
+                httpClient, cancellationToken
+                );
+        }
 
-        public static Task<MicManifest> GetMicManifest(Uri manifestServiceUri, string hostname, HttpClient httpClient, CancellationToken cancellationToken = default)
+        public static Task<MicManifest> GetMicManifest(
+            Uri manifestServiceUri, string hostname, HttpClient httpClient,
+            CancellationToken cancellationToken = default)
         {
             if (manifestServiceUri == null)
                 throw new ArgumentNullException(nameof(manifestServiceUri));
             if (httpClient == null)
             {
-                using (httpClient = new HttpClient())
-                {
-                    return GetMicManifestInternal(manifestServiceUri, hostname, httpClient, cancellationToken);
-                }
+                return GetMicManifestInternal(manifestServiceUri, hostname, cancellationToken);
             }
 
             return GetMicManifestInternal(manifestServiceUri, hostname, httpClient, cancellationToken);
         }
 
-        private static async Task<MicManifest> GetMicManifestInternal(Uri manifestServiceUri, string hostname, HttpClient httpClient, CancellationToken cancellationToken = default)
+        private static async Task<MicManifest> GetMicManifestInternal(
+            Uri manifestServiceUri, string hostname,
+            CancellationToken cancellationToken)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                return await GetMicManifestInternal(
+                    manifestServiceUri, hostname, httpClient, cancellationToken
+                    ).ConfigureAwait(continueOnCapturedContext: false);
+            }
+        }
+
+        private static async Task<MicManifest> GetMicManifestInternal(
+            Uri manifestServiceUri, string hostname, HttpClient httpClient,
+            CancellationToken cancellationToken)
         {
             var requestUri = new Uri(manifestServiceUri, "?hostname=" + Uri.EscapeDataString(hostname));
             using (var response = await httpClient.GetAsync(requestUri, cancellationToken).ConfigureAwait(continueOnCapturedContext: false))
