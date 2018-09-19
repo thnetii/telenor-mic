18bf7016f9e872d0713f2bbeb14371fdfa1f7eb3
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-09-19 19:58:25 +0200

    Start work on Cloud API class library

---
+++ src/TelenorConnexion.ManagedIoTCloud.CloudApi/AuthApi/AuthLoginRequest.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace TelenorConnexion.ManagedIoTCloud.CloudApi.AuthApi
+{
+    public class AuthLoginRequest : CloudApiRequest<AuthLoginRequestAttributes>
+    {
+    }
+
+    public class AuthLoginRequestAttributes
+    {
+        public const string Action = "LOGIN";
+
+        [JsonProperty("userName")]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    }
+}
---
+++ src/TelenorConnexion.ManagedIoTCloud.CloudApi/CloudApiErrorMessage.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace TelenorConnexion.ManagedIoTCloud.CloudApi
+{
+    public class CloudApiErrorMessage
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("messageKey")]
+        public string MessageKey { get; set; }
+
+        [JsonProperty("messageParams")]
+        public IDictionary<string, JToken> Parameters { get; set; }
+
+        [JsonProperty("property")]
+        public string Property { get; set; }
+    }
+}
