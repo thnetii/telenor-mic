7dbb5f030b95f6eb3cd0b15aace63e9ea927ea5f
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-10-10 16:46:53 +0200

    Add DocFx for documentation

--- sample/TelenorConnexion.ManagedIoTCloud.Sample.Cmd/Program.cs
+++ sample/TelenorConnexion.ManagedIoTCloud.Sample.Cmd/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Amazon.IoTDeviceGateway;
-using McMaster.Extensions.CommandLineUtils;
 using MQTTnet;
 using MQTTnet.Client;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Linq;
 using System.Net;
@@ -12,29 +12,24 @@ using System.Threading.Tasks;
 using TelenorConnexion.ManagedIoTCloud.LambdaClient;
 using TelenorConnexion.ManagedIoTCloud.Model;
 using TelenorConnexion.ManagedIoTCloud.RestClient;
+using THNETII.Common;
+using THNETII.Common.Cli;
 
 namespace TelenorConnexion.ManagedIoTCloud.Sample.Cmd
 {
     public static class Program
     {
-        public const bool useProxy = true;
+        public const bool useProxy = false;
         public const bool useLambdaClient = false;
 
-        public static async Task Main(string[] args)
+        public static int Main()
         {
-            CancellationTokenSource cts = new CancellationTokenSource();
-            Console.CancelKeyPress += (sender, e) =>
-            {
-                cts.Cancel(throwOnFirstException: true);
-                e.Cancel = true;
-            };
-
-            try { await Run(cts.Token); }
-            catch (OperationCanceledException) { }
-            catch (AggregateException aggregateExcept) when (aggregateExcept.InnerException is OperationCanceledException) { }
+            try { ConsoleUtils.RunAsync(Run).GetAwaiter().GetResult(); }
+            catch (OperationCanceledException) { return ProcessExitCode.ExitFailure; }
+            return ProcessExitCode.ExitSuccess;
         }
 
-        public static async Task<MicClient> CreateMicClient(string hostname, string apiKey, CancellationToken cancelToken)
+        public static async Task<MicClient> CreateMicClient(string hostname, CancellationToken cancelToken)
         {
             HttpClientHandler httpHandler = new HttpClientHandler();
             if (useProxy)
@@ -43,10 +38,10 @@ namespace TelenorConnexion.ManagedIoTCloud.Sample.Cmd
                 httpHandler.UseProxy = true;
             }
 
-            MicManifest manifest;
             MicClient micClient;
             if (useLambdaClient)
             {
+                MicManifest manifest;
                 using (var httpClient = new HttpClient(httpHandler))
                 {
                     manifest = await MicManifest.GetMicManifest(hostname, httpClient, cancelToken);
@@ -55,6 +50,8 @@ namespace TelenorConnexion.ManagedIoTCloud.Sample.Cmd
             }
             else
             {
+                Console.Write("API Key: ");
+                string apiKey = await ConsoleUtils.ReadLineAsync(cancelToken);
                 micClient = await MicRestClient.CreateFromHostname(hostname, apiKey, httpHandler, cancelToken);
             }
 
@@ -70,23 +67,18 @@ namespace TelenorConnexion.ManagedIoTCloud.Sample.Cmd
         public static async Task Run(CancellationToken cancelToken)
         {
             Console.Write("MIC Hostname: ");
-            var hostname = Console.ReadLine();
-            string apiKey = null;
-            if (!useLambdaClient)
-            {
-                Console.Write("API Key: ");
-                apiKey = Console.ReadLine();
-            }
+            var hostname = await ConsoleUtils.ReadLineAsync(cancelToken);
             Console.WriteLine("Getting MIC manifest . . .");
             using (var proxyHandler = new HttpClientHandler() { Proxy = new WebProxy("http://localhost:8888/"), UseProxy = true })
-            using (var micClient = await CreateMicClient(hostname, apiKey, cancelToken))
+            using (var micClient = await CreateMicClient(hostname, cancelToken))
             {
                 var micClientConfig = micClient.Config;
                 micClientConfig.LogMetrics = true;
 
                 Console.Write("Username: ");
-                var username = Console.ReadLine();
-                var password = Prompt.GetPassword("Password:");
+                string username = await ConsoleUtils.ReadLineAsync(cancelToken);
+                Console.Write("Password: ");
+                string password = await ConsoleUtils.ReadLineMaskedAsync(cancelToken);
 
                 Console.Write("Logging in . . . ");
                 var login = await micClient.AuthLogin(
@@ -95,13 +87,29 @@ namespace TelenorConnexion.ManagedIoTCloud.Sample.Cmd
                 Console.WriteLine("Successful!");
                 Console.WriteLine();
 
+                var awsCredentials = ((IMicClient)micClient).AwsCredentials;
+                Console.WriteLine($"Cognito Identity pool: {awsCredentials.IdentityPoolId}");
+                Console.WriteLine($"  Identity Id: {login.Credentials.IdentityId}");
+                Console.WriteLine($"  Login provider: {micClient.Manifest.GetCognitoProviderName()}");
+                Console.WriteLine($"  Token: {login.Credentials.Token}");
+                Console.WriteLine();
+
+                var immutableCredentials = await awsCredentials.GetCredentialsAsync();
+                Console.WriteLine("AWS Immutable Credentials:");
+                Console.WriteLine($"  Access key: {immutableCredentials.AccessKey}");
+                Console.WriteLine($"  Secret key: {immutableCredentials.SecretKey}");
+                if (immutableCredentials.UseToken)
+                    Console.WriteLine($"  Token:      {immutableCredentials.Token}");
+                Console.WriteLine();
+
                 var userInfo = await micClient.UserGet(login.User.Username, cancelToken);
                 Console.WriteLine(JsonConvert.SerializeObject(userInfo, Formatting.Indented));
                 Console.WriteLine();
 
+                Console.WriteLine($"IoT Endpoint: {micClient.Manifest.IotEndpoint}");
                 Console.Write("Connecting MQTT Client . . . ");
                 var iotConfig = micClientConfig.Create<AmazonIoTDeviceGatewayConfig>();
-                using (var iotClient = new AmazonIoTDeviceGatewayClient(((IMicClient)micClient).AwsCredentials, iotConfig))
+                using (var iotClient = new AmazonIoTDeviceGatewayClient(awsCredentials, iotConfig))
                 {
                     var mqttOptionsTask = iotClient.CreateMqttWebSocketClientOptionsAsync(micClient.Manifest.IotEndpoint, cancelToken);
 
@@ -121,7 +129,14 @@ namespace TelenorConnexion.ManagedIoTCloud.Sample.Cmd
                                     int payloadLength = e.ApplicationMessage.Payload?.Length ?? 0;
                                     Console.WriteLine($"  Message Payload: ({payloadLength} byte{(payloadLength == 1 ? "" : "s")})");
                                     Console.WriteLine();
-                                    Console.WriteLine(e.ApplicationMessage.ConvertPayloadToString());
+                                    string payload = e.ApplicationMessage.ConvertPayloadToString();
+                                    try
+                                    {
+                                        var jtoken = JToken.Parse(payload);
+                                        payload = jtoken.ToString(Formatting.Indented);
+                                    }
+                                    catch { }
+                                    Console.WriteLine(payload);
                                     Console.WriteLine();
                                     Console.WriteLine(new string('-', count: 20));
                                     Console.WriteLine();
