ccd747fa9e138f6bf3912621965ce689a16b9285
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-10-31 15:58:32 +0100

    Extend Counter component: Add Reset and Decrement

--- src/TelenorConnexion.ManagedIoTCloud.GraphQL.Playground/ClientApp/src/store/Counter.js
+++ src/TelenorConnexion.ManagedIoTCloud.GraphQL.Playground/ClientApp/src/store/Counter.js
@@ -1,9 +1,11 @@
 ﻿const incrementCountType = 'INCREMENT_COUNT';
+const resetCountType = 'RESET_COUNT';
 const decrementCountType = 'DECREMENT_COUNT';
 const initialState = { count: 0 };
 
 export const actionCreators = {
   increment: () => ({ type: incrementCountType }),
+  reset: () => ({ type: resetCountType }),
   decrement: () => ({ type: decrementCountType })
 };
 
@@ -12,6 +14,10 @@ export const reducer = (state, action) => {
 
   if (action.type === incrementCountType) {
     return { ...state, count: state.count + 1 };
+  }
+
+  if (action.type === resetCountType) {
+    return { ...state, count: initialState.count };
   }
 
   if (action.type === decrementCountType) {
