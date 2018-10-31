d09673cf4ae1e1bb35106cabc80661a1acd360ed
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-10-31 20:02:55 +0100

    refactor graphqlApolloLink

--- src/TelenorConnexion.ManagedIoTCloud.GraphQL.Playground/ClientApp/src/components/GraphqlPlayground.js
+++ src/TelenorConnexion.ManagedIoTCloud.GraphQL.Playground/ClientApp/src/components/GraphqlPlayground.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import { Provider, connect } from 'react-redux';
 import { Playground, store } from 'graphql-playground-react';
-import { Session } from 'graphql-playground-react/lib/state/sessions/reducers';
 import { defaultLinkCreator } from 'graphql-playground-react/lib/state/sessions/fetchingSagas';
-import { HttpLink } from 'apollo-link-http';
 
-/**
- * @param {Session} session Current session
- * @param {string?} wsEndpoint WebSocket endpoint
- * @returns {HttpLink} the link to GraphQL server
- */
 function graphqlApolloLink(session, wsEndpoint) {
 
   let { headers } = session;
 
-  if (typeof headers !== 'object') {
-    headers = {};
-  }
-  for (let key in headers) {
-    if (key.toUpperCase() === 'X-APOLLO-TRACING') {
-      delete headers[key];
-      break;
+  if (typeof headers === 'object') {
+    for (let key in headers) {
+      if (key.toUpperCase() === 'X-APOLLO-TRACING') {
+        delete headers[key];
+        break;
+      }
     }
   }
-  session.headers = headers;
 
   console.log('graphqlApolloLink', session);
   let apolloLink =  defaultLinkCreator(session, wsEndpoint);
